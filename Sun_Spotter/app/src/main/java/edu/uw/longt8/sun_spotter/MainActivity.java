692aafd1ac7084788863d376393d1ed099c6cdf5
leontaolong <longt8@uw.edu>
2017-04-11 21:38:12 -0700

    Add input search layout

---
+++ Sun_Spotter/app/src/main/java/edu/uw/longt8/sun_spotter/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.uw.longt8.sun_spotter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final EditText searchField = (EditText)findViewById(R.id.txtSearch);
+        final Button searchButton = (Button)findViewById(R.id.btnSearch);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchTerm = searchField.getText().toString();
+                Log.v(TAG, "Searching for: " + searchTerm);
+
+//                MovieDownloadTask task = new MovieDownloadTask();
+//                task.execute(searchTerm);
+            }
+        });
+    }
+}
